ea43c176b5ef1eab842f6fbc8a2e7e4c0f4295e5
Sugar-Ochir <sugarochir2k@gmail.com>
2025-08-14 16:48:29 +1000

    added problem description

--- linkedlist/reverseLinkedList.java
+++ linkedlist/reverseLinkedList.java
@@ -1,3 +1,18 @@
+/* Och1r1
+Problem Description:
+
+Given the beginning of a singly linked list head, 
+reverse the list, and return the new beginning of the list.
+
+Oh god i hate linkedlist so much, i had so much problem with easy linkedlist problem :((
+
+Example 1:
+
+Input: head = [0,1,2,3]
+
+Output: [3,2,1,0]
+ */
+
 class ListNode {
     int val;
     ListNode next;
