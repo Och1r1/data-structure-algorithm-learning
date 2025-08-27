9292dbb764e618545b5d976eb6c5d4e769c7990b
Sugar-Ochir <sugarochir2k@gmail.com>
2025-08-27 23:07:53 +1000

    adding sum of binary tree using queue by BFS algorithm

--- tree/sumOfBinaryTree.java
+++ tree/sumOfBinaryTree.java
@@ -1,5 +1,6 @@
 // so i thought before solving leetcode tree questions, i need to understand at least basic questions about tree to familiriaze with my self.
 // recursion version:
+/*
 public class sumOfBinaryTree {
     public int sum(TreeNode root){
         if(root == null){
@@ -12,7 +13,40 @@ public class sumOfBinaryTree {
             return root.val + leftSum + rightSum;
         }
     }
+}*/
+
+/*
+✅ So:
+DFS → recursion OR stack - DFS gedeg cin preorder l gsen ug, ehleed neg tree ruugee shumbana
+BFS → queue - Level-by-level, level eer n yvna gsen ug
+ */
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class sumOfBinaryTree{
+    public int sum(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        Queue<TreeNode> qu = new LinkedList<>();
+        qu.add(root);
+        int total = 0;
+
+        while(!qu.isEmpty()){
+            TreeNode cur = qu.poll();
+            total += cur.val;
+            if(cur.left != null) qu.add(cur.left);
+            if(cur.right != null) qu.add(cur.right);
+        }
+
+        return total;
+    }
 }
+
+
+
 
 class TreeNode {
     int val;
