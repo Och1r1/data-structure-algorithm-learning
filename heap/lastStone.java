2364466c134d49f47550aec1926871f48d43ddbc
Sugar-Ochir <sugarochir2k@gmail.com>
2025-09-10 23:42:23 +1000

    added last stone but being slow

---
+++ heap/lastStone.java
@@ -0,0 +1,28 @@
+import java.util.PriorityQueue;
+
+public class lastStone {
+    public static int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for(int stone: stones){
+            pq.offer(stone);
+        }
+
+        while(!pq.isEmpty()){
+            if(pq.size() == 1){
+                return pq.poll();
+            }
+            int max = pq.poll();
+            int second = pq.poll();
+            if(max - second > 0){
+                pq.add(max - second);
+            }
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args){
+        int[] ls = {2, 3, 6, 2, 4};
+        System.out.println(lastStoneWeight(ls));
+    }
+}
